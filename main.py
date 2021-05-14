2e4a8f50732c705d0a4983c2e078aa29d501e2b6
Umang <umang.rke333@gmail.com>
2021-05-14 20:49:45 +0200

    Added new image

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
+GREY = "#9ea2ab"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -76,19 +77,19 @@ def count_down(count):
 
 window = Tk()
 window.title("Break Time")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=100, pady=50, bg=GREY)
 
 
 
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness = 0)
-tomato_img = PhotoImage(file= "tomato.png")
+canvas = Canvas(width=200, height=224, bg=GREY, highlightthickness = 0)
+tomato_img = PhotoImage(file= "monster.png")
 canvas.create_image(100,112,  image = tomato_img)
 
-label = Label(text = "Timer", font=(FONT_NAME, 35, "bold"), bg=YELLOW)
+label = Label(text = "Timer", font=(FONT_NAME, 35, "bold"), bg=GREY)
 label.grid(column=2, row=1)
 
-timer_text = canvas.create_text(100, 112, text="00:00", fill="yellow", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 112, text="00:00", fill="purple", font=(FONT_NAME, 28, "bold"))
 canvas.grid(column=2, row=2)
 
 
